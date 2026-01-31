5ce65834826e88bf636094605b139d94bd10629b
lacyfotocopychapai-jpg <lacyfotocopychapai@gmail.com>
2026-01-31 20:18:14 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,11 @@ session_memory = {"facts": []}
 
 @app.route('/')
 def index():
-    return send_from_directory('web_content', 'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
 
 @app.route('/chat', methods=['POST'])
 def handle_chat():
